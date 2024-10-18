da0c66b7fe8a40688d1307196c81f1dc24926809
DannyPhongcoderso1 <dannyphong2407@gmail.com>
2024-10-18 19:54:42 +0700

    Add project files.

---
+++ projectending/Employee.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectending
+{
+    public class Employee
+    {
+        private string name;
+        private string employeeID;
+        private string roleID;
+        private string departmentID;
+        private decimal salary;
+        private string dateofhire;
+        private int paidleave;
+
+     
+        public string EmployeeID() { return employeeID; }
+      
+        public string RoleID() { return roleID; }
+       
+        public string DepartmentID() { return departmentID; }
+        
+        public decimal Salary() { return salary; }
+        
+        public string DateOfHire() {return dateofhire;}
+    
+
+
+        public Employee(string name,string employeeID, string roleID, string departmentID, decimal salary, string dateofhire)
+        {
+            this.employeeID = employeeID;
+            this.roleID = roleID;
+            this.departmentID = departmentID;
+            this.salary = salary;
+            this.dateofhire = dateofhire;    
+
+        }
+        public void Absent(List<Employee> employees)
+        {
+            
+            Console.WriteLine("Nhap ma nhan vien: ");
+            string absentemployeeID = Console.ReadLine();
+            foreach (Employee employee in employees)
+            {
+                if(employee.EmployeeID() == absentemployeeID)
+                {
+                    employee.paidleave++;
+                }    
+            }
+        } 
+
+        public int numAbsent(List<Employee> employees, string employeeID)
+        {
+            foreach (Employee employee in employees)
+
+            {
+                if(employee.EmployeeID() ==  employeeID)
+                    return employee.paidleave;
+            }
+            return 0;
+        }
+        
+
+        public void Add(List<Employee> employee)
+        {
+            Console.Write("nhap ten: ");
+            name = Console.ReadLine();
+            Console.Write("nhap ma cong nhan: ");
+            employeeID = Console.ReadLine();
+            Console.Write("nhap ma chuc vu: ");
+            roleID = Console.ReadLine();
+            Console.Write("nhap ma phong ban: ");
+            departmentID = Console.ReadLine();
+            Console.Write("nhap tien luong");
+            salary = decimal.Parse(Console.ReadLine());
+            Console.Write("Nhap ngay vao lam: ");
+            dateofhire = Console.ReadLine();
+
+
+            employee.Add(new Employee(name, employeeID, roleID, departmentID, salary, dateofhire));
+        }
+
+        public void Remove(List<Employee> employee)
+        {
+            Console.Write("Nhap ma nhan vien ban muon xoa: ");
+            string employeeIDtodelete = Console.ReadLine();
+            foreach(Employee emp in employee)
+            {
+                if(emp.employeeID == employeeIDtodelete)
+                {
+                    employee.Remove(emp);
+                }
+            }
+        }
+
+        public void Update(List<Employee> employee)
+        {
+            Console.Write("Nhap ma nhan vien ban muon chinh sua: ");
+            string employeeIDtoupdate = Console.ReadLine();
+            foreach (Employee emp in employee)
+            {
+                if(emp.employeeID == employeeIDtoupdate)
+                {
+                    Console.Write("Nhap vi tri ban muon sua: ");
+                    int vitri = int.Parse(Console.ReadLine());
+                    switch(vitri)
+                    {
+                        case 1:
+                            {
+                                Console.Write("nhap ten moi: ");
+                                string tenmoi = Console.ReadLine();
+                                emp.name = tenmoi;
+                            }
+                            break;
+                        case 2:
+                            {
+                                Console.Write("nhap ma nhan vien moi: ");
+                                string newemployeeid = Console.ReadLine();
+                                emp.employeeID = newemployeeid;
+                            }
+                            break;
+                        case 3:
+                            {
+                                Console.Write("nhap ma chuc vu moi: ");
+                                string newroleid = Console.ReadLine();
+                                emp.roleID = newroleid;
+                            }
+                            break;
+                        case 4:
+                            {
+                                Console.Write("nhap ma phong ban moi: ");
+                                string newdepartmentid = Console.ReadLine();
+                                emp.departmentID = newdepartmentid;
+                            }
+                            break;
+                        case 5:
+                            {
+                                Console.Write("nhap so tien luong moi: ");
+                                decimal newsalary = decimal.Parse(Console.ReadLine());
+                                emp.salary = newsalary;
+                            }
+                            break;
+
+                        case 6:
+                            {
+                                Console.Write("nhap ngay vao lam moi");
+                                string newdate = Console.ReadLine();
+                                emp.dateofhire = newdate;
+                            }
+                            break;
+                    }
+                }    
+            }    
+        }
+    }
+}
---
+++ projectending/Role.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectending
+{
+    public class Role
+    {
+        private string roleID;
+        private string rolename;
+        private string permission;
+        public bool checkexist = false;
+  
+        public Role(string roleID, string rolename, string permission)
+        {
+            this.roleID = roleID;
+            this.rolename = rolename;
+            this.permission = permission;
+        }
+
+        public string checkroleID(List<Employee> employee, List<Role>role)
+        {
+            Console.Write("Nhap ma chuc vu ");
+            string x = Console.ReadLine();
+            foreach (Employee emp in employee)
+            {
+
+
+                foreach (Role role1 in role)
+                {
+
+
+                    if (emp.EmployeeID() == role1.roleID)
+                    {
+                        return role1.rolename;
+                    }
+                }
+            }
+            return "Mã chức vụ không tồn tại";
+            
+        }
+
+        public void addpermission() 
+        {
+
+        }
+
+        public void removepermission() 
+        {
+
+        }
+
+        public void Updatepermission()
+        {
+
+        }
+
+    }
+}
